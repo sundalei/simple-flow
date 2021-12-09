ab7aa1415229b4531c2602f0e943583115d2b2b6
DaLei Sun <dalei@DaLeis-MacBook-Pro.local>
2021-12-09 22:41:53 +0800

    simple flow

---
+++ src/main/java/sia5/FileWriterIntegrationConfig.java
@@ -0,0 +1,14 @@
+package sia5;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Profile("xmlconfig")
+    @Configuration
+    @ImportResource("classpath:filewriter-config.xml")
+    public static class XmlConfiguration {}
+}
