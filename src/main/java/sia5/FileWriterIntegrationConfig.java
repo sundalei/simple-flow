d7ccb74684f51f13d31068760d8c9e5b115c1a90
DaLei Sun <dalei@DaLeis-MacBook-Pro.local>
2021-12-11 11:55:43 +0800

    flow java config

--- src/main/java/sia5/FileWriterIntegrationConfig.java
+++ src/main/java/sia5/FileWriterIntegrationConfig.java
@@ -1,8 +1,16 @@
 package sia5;
 
+import java.io.File;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
 
 @Configuration
 public class FileWriterIntegrationConfig {
@@ -11,4 +19,25 @@ public class FileWriterIntegrationConfig {
     @Configuration
     @ImportResource("classpath:filewriter-config.xml")
     public static class XmlConfiguration {}
+
+    @Profile("javaconfig")
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Profile("javaconfig")
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler =
+            new FileWritingMessageHandler(new File("/Users/dalei/folder"));
+
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+
+        return handler;
+    }
 }
